a7e0c039cba05b8b59b5cafdf827c9a4f12b5739
Team Webgalli <webgalli@gmail.com>
2012-07-17 00:37:51 +0530

    Added credits

--- start.php
+++ start.php
@@ -17,15 +17,17 @@ function galliShortUrl_init() {
 	elgg_extend_view('js/elgg', 'galliShortUrl/js');
     elgg_register_ajax_view('galliShortUrl/short');	
 }
-
+/**
+* To know about this function see
+* http://www.webgalli.com/blog/easily-create-short-urls-with-php-curl-and-goo-gl-or-bit-ly/
+*/
 function goo_gl_short_url($longUrl) {
-	$GoogleApiKey = 'enter-your-google-api-key-here';
+	$GoogleApiKey = elgg_get_plugin_setting('googleApiKey', 'galliShortUrl');
 	$postData = array('longUrl' => $longUrl, 'key' => $GoogleApiKey);
 	$jsonData = json_encode($postData);
 	$curlObj = curl_init();
 	curl_setopt($curlObj, CURLOPT_URL, 'https://www.googleapis.com/urlshortener/v1/url');
 	curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
-	//As the API is on https, set the value for CURLOPT_SSL_VERIFYPEER to false. This will stop cURL from verifying the SSL certificate.
 	curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
 	curl_setopt($curlObj, CURLOPT_HEADER, 0);
 	curl_setopt($curlObj, CURLOPT_HTTPHEADER, array('Content-type:application/json'));
